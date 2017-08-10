c258a693db4d465a1663034ae19e186b536e13aa
robertw <r.wdowczyk@gmail.com>
2017-08-10 22:32:25 +0200

    zadanie 9

---
+++ scripts.js
@@ -0,0 +1,10 @@
+﻿// scripts.js
+function getTriangleArea (a, h) {
+  var TriangleArea = a*h/2;
+  if ((a <= 0) || (h <= 0)) {
+    console.log("Nieprawidłowe dane");
+    return; }
+    return TriangleArea;
+  }
+console.log(getTriangleArea(10, 6) );
+var triangle1Area = getTriangleArea(10, 15);
