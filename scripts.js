cb164ba2ace77b8de9caabd9f3a5bc93cd32afbc
robertwdowczyk <30527317+robertwdowczyk@users.noreply.github.com>
2017-08-21 18:46:13 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
 ﻿// scripts.js
-function getTriangleArea (a, h) {
-  var triangleArea = a*h/2;
-  if ((a <= 0) || (h <= 0)) {
-    return ("Nieprawidłowe dane");
-}
-    return triangleArea;
-  
-console.log(getTriangleArea(10, 6) );
-var triangle1Area = getTriangleArea(10, 15);
+var txt ='Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.'
+var dinosaur = txt.replace('Velociraptor', 'Triceratopsa');
+console.log(dinosaur);
+var dinosaurUpperCased = dinosaur.toUpperCase();
+console.log(dinosaurUpperCased);
+var dinosaur = txt.replace('Velociraptor', 'Dinosaur')
+var partOfDinosaur = dinosaur.slice(lenght/2);
+console.log(partOfDinosaur);
