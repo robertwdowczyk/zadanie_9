16ea0d24279bb7d23890082e9a01c3a59c8a51e7
robertwdowczyk <30527317+robertwdowczyk@users.noreply.github.com>
2017-08-26 18:18:49 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 ﻿// scripts.js
-var button = document.getElementsByClassName('button');
-
-for (var i = 0; i < button.length; i++ ) {
-  var butt = button[i].innerHTML;
-  alert(butt);
- 
-}
+var list = document.getElementById('list');
+var add = document.getElementById('addElem');
+add.addEventListener('click', function() {
+  var element = document.createElement('li');
+  var x = document.getElementsByTagName('li').length;
+  element.innerHTML = 'item ' + x ;
+  
+  list.appendChild(element);
+});
